822c7614ce44dd52b761f28c947e5497ef6f59b1
Matthew Napolitano <mnapolitano@wl-mce-01.(none)>
2012-01-31 15:17:44 -0700

    First testable commit.

--- kmirror.py
+++ kmirror.py
@@ -14,7 +14,7 @@
 kmirror.py created on 01/18/2012 by Matt Napolitano.
 ----------------------------------------------------
 This is a GUI designed to aid the testing of the Newport rotation stage RV350PP through the Newport XPS-C8 controller.
-This program will allow for absolute moving, relative moving, continuous rotation as well as speed variation.
+This program will allow for absolute moving, relative moving, and speed variation.
 For full details refer to README_KMIRROR.
 ----------------------------------------------------
 '''
@@ -24,13 +24,7 @@ import sys
 import wx.lib.agw.floatspin as FS
 #import XPS_C8_drivers as xps
 
-def XPSErrorHandler(Ecode):
-	self.code=Ecode
-######
-######
-######
-######
-######
+
 
 class KMirrorApp(wx.App):
 	'''The K-Mirror testing app.'''	
@@ -44,33 +38,25 @@ class KMirrorFrame(wx.Frame):
 	'''The frame for the K-Mirror panel.'''
 	def __init__(self,*args,**kwargs):
 		super(KMirrorFrame,self).__init__(*args,**kwargs)
-		self.run_mode=0
-		#self.x=xps.XPS()
-		#self.SocketID=x.TCP_ConnectToServer('192.168.0.254',5001,1)
-		#if self.SocketID == -1:
-		#	self.run_mode = -1
-		#
 		self.panel_zero=Master(self)
 		self.panel_one=Information(self)
 		self.panel_two=Control(self)
 		self.line_zero=wx.StaticLine(self,style=wx.LI_HORIZONTAL)
-		self.line_one=wx.StaticLine(self,style=wx.LI_VERTICAL)
+		self.line_one=wx.StaticLine(self,style=wx.LI_HORIZONTAL)
 		
 		self.__DoLayout()
 		self.SetInitialSize()
 		self.Bind(wx.EVT_CLOSE,self.OnClose)
-		
-		if self.run_mode == -1:
-			self.OnFail()
 
 	def __DoLayout(self):
 		'''A basic layout handler.'''
 		sizer=wx.GridBagSizer()
-		sizer.Add(self.panel_zero,(0,0),(1,3),wx.EXPAND,border=5)  
-		sizer.Add(self.line_zero,(1,0),(1,3),wx.EXPAND,border=5)
-		sizer.Add(self.panel_one,(2,0),border=5)
-		sizer.Add(self.line_one,(2,1),border=5)
-		sizer.Add(self.panel_two,(2,2),border=5)
+		sizer.Add(self.panel_two,(0,0),(1,1),wx.ALIGN_CENTER_HORIZONTAL|wx.CENTER)
+		sizer.Add(self.line_zero,(1,0),(1,1),wx.EXPAND,border=5)
+		sizer.Add(self.panel_one,(2,0),(1,1),wx.ALIGN_CENTER_HORIZONTAL|wx.CENTER)  
+		sizer.Add(self.line_one,(3,0),(1,1),wx.EXPAND,border=5)
+		sizer.Add(self.panel_zero,(4,0),(1,1),wx.ALIGN_CENTER_HORIZONTAL|wx.CENTER)
+		
 		self.SetSizer(sizer)
 
 	def OnClose(self,event):
@@ -84,37 +70,27 @@ class KMirrorFrame(wx.Frame):
 			self.panel_zero.Close()
 			self.panel_one.Close()
 			self.panel_two.Close()
-			#self.x.TCP_CloseSocket(SocketID)
 		else:
 			event.Veto()
-
-	def OnFail(self):
-		result=wx.MessageBox('Connection to Newport Controller has failed.\nPlease Check IP and Port and restart program.',style=wx.CENTER|wx.ICON_EXCLAMATION|wx.OK)
-
-		if result == wx.OK:
-			sys.exit()
-
-		else:
-			sys.exit()
-
 	
 class Master(wx.Panel):
 	'''The Master controller panel, which changes states for the program and device.'''
 	def __init__(self,*args,**kwargs):
 		super(Master,self).__init__(*args,**kwargs)
 		self.mode='Enable'
-		self.title=wx.StaticText(self,label='Master')
-		self.move_enable=wx.Button(self,label='%s' % self.mode)
-		self.kill_group=wx.Button(self,label='kill group')
-		#####################################
+		self.title=wx.StaticText(self,label='Emergency')
+		self.kill_group=wx.Button(self,label='KILL ALL')
 		self.kill_group.SetBackgroundColour(wx.Colour(255,0, 0))
 		self.kill_group.ClearBackground()
 		self.kill_group.Refresh()   
-		#self.x=xps.XPS()
-		#self.SocketID=x.TCP_ConnectToServer('192.168.0.254',5001,1)
-		#if self.SocketID == -1:
-		#	self.run_mode = -1
-		#
+		#####################################
+		'''
+		self.x=xps.XPS()
+		self.SocketID=x.TCP_ConnectToServer('192.168.0.254',5001,1)
+		if self.SocketID == -1:
+			self.OnFail()
+		
+		'''
 		#####################################
 		self.line=wx.StaticLine(self,style=wx.LI_HORIZONTAL)
 		self.__DoLayout()
@@ -122,33 +98,64 @@ class Master(wx.Panel):
 		self.SetInitialSize()
 		
 	def __DoLayout(self):
-		'''A basic layout handler for Master Panel.'''
+		'''A basic layout handler for Emergency Panel.'''
 		sizer=wx.GridBagSizer()
-		sizer.Add(self.title,(0,0),(1,5),wx.ALIGN_CENTER_HORIZONTAL|wx.CENTER)
-		sizer.Add(self.line,(1,0),(1,4),wx.EXPAND)
-		sizer.Add(self.move_enable,(2,1))
-		sizer.Add(self.kill_group,(2,3))
+		sizer.Add(self.title,(0,0),(1,1),wx.ALIGN_CENTER_HORIZONTAL)
+		sizer.Add(self.line,(1,0),(1,1),wx.EXPAND)
+		sizer.Add(self.kill_group,(2,0))
 		self.SetSizer(sizer)
 
 	def OnButton(self,event):
-		if event.GetEventObject()==self.move_enable:
-			pass################
+		pass
+		#kill=self.x.KillAll(SocketID)
+		#if kill[0] != 0:
+		#	XPSErrorHandler(self.SocketID, self.kill[0], 'KillAll')
+		#else:
+		#	result=wx.MessageBox('All Groups Killed.\nProgram Must Be Restarted.',style=wx.CENTER|wx.ICON_EXCLAMATION|wx.OK)		
+			
+	def OnFail(self):
+		result=wx.MessageBox('Connection to Newport Controller has failed at Emergency Panel.\nPlease Check IP and Port.',style=wx.CENTER|wx.ICON_EXCLAMATION|wx.OK)
+
+		if result == wx.OK:
+			sys.exit()
+
 		else:
-			pass################
+			sys.exit()
 
 	def Close(self):
-		print 'red leader'
 		#self.x.TCP_CloseSocket(SocketID)
+		
+		print 'Emergency Panel Closed'
+
+	def XPSErrorHandler(self,socket,code,name):
+		if code != -2 and code != -108:
+			error=self.x.ErrorStringGet(socket,code)
+			if error[0] != 0:
+				choice=wx.MessageBox(name +' : ERROR '+ str(code),style=wx.CENTER|wx.ICON_EXCLAMATION|wx.OK)
+			else:
+				choice=wx.MessageBox(name +' : '+ error[1],style=wx.CENTER|wx.ICON_EXCLAMATION|wx.OK)
+		else:
+			if code == -2:
+				choice=wx.MessageBox(name +' : TCP timeout',style=wx.CENTER|wx.ICON_EXCLAMATION|wx.OK)
+			elif code == -108:
+				choice=wx.MessageBox(name +' : The TCP/IP connection was closed by an administrator',style=wx.CENTER|wx.ICON_EXCLAMATION|wx.OK)
+	
 
 class Information(wx.Panel):
 	def __init__(self,*args,**kwargs):
 		super(Information,self).__init__(*args,**kwargs)
 		self.title=wx.StaticText(self,label='Information')
-		#self.x=xps.XPS()
-		#self.SocketID=x.TCP_ConnectToServer('192.168.0.254',5001,1)
-		#if self.SocketID == -1:
-		#	self.run_mode = -1
-		#
+		###################################
+		'''
+		self.x=xps.XPS()
+		self.SocketID=x.TCP_ConnectToServer('192.168.0.254',5001,1)
+		if self.SocketID == -1:
+			self.OnFail()
+			
+		self.Group = 'Group1'
+		self.Positioner = self.Group + '.Positioner'
+		'''
+		###################################
 		self.__DoLayout()
 		self.SetInitialSize()
 
@@ -158,9 +165,32 @@ class Information(wx.Panel):
 		sizer.Add(self.title,(0,0))
 		self.SetSizer(sizer)
 
+	def OnFail(self):
+		result=wx.MessageBox('Connection to Newport Controller has failed at Information Panel.\nPlease Check IP and Port.',style=wx.CENTER|wx.ICON_EXCLAMATION|wx.OK)
+
+		if result == wx.OK:
+			sys.exit()
+
+		else:
+			sys.exit()
+
+
 	def Close(self):
 		print 'panel_one'
 		#self.x.TCP_CloseSocket(SocketID)
+
+	def XPSErrorHandler(self,socket,code,name):
+		if code != -2 and code != -108:
+			error=self.x.ErrorStringGet(socket,code)
+			if error[0] != 0:
+				choice=wx.MessageBox(name +' : ERROR '+ str(code),style=wx.CENTER|wx.ICON_EXCLAMATION|wx.OK)
+			else:
+				choice=wx.MessageBox(name +' : '+ error[1],style=wx.CENTER|wx.ICON_EXCLAMATION|wx.OK)
+		else:
+			if code == -2:
+				choice=wx.MessageBox(name +' : TCP timeout',style=wx.CENTER|wx.ICON_EXCLAMATION|wx.OK)
+			elif code == -108:
+				choice=wx.MessageBox(name +' : The TCP/IP connection was closed by an administrator',style=wx.CENTER|wx.ICON_EXCLAMATION|wx.OK)
 
 class Control(wx.Panel):
 	def __init__(self,*args,**kwargs):
@@ -171,28 +201,43 @@ class Control(wx.Panel):
 		self.label_two=wx.StaticText(self,label='deg/s')
 		self.label_three=wx.StaticText(self,label='Travel Position')
 		self.label_four=wx.StaticText(self,label='deg')
-		self.speed=FS.FloatSpin(self,digits=5)
-		self.position=FS.FloatSpin(self,digits=5)
+		self.speed=FS.FloatSpin(self,digits=6)
+		self.position=FS.FloatSpin(self,digits=6)
 
 		self.mode_one=wx.RadioButton(self,-1,'Move Relative  ', style = wx.RB_GROUP)
 		self.mode_two=wx.RadioButton(self,-1,'Move Absolute  ')
-		self.mode_three=wx.RadioButton(self,-1,'Move Spindle  ')
 		
-		self.move_mode=0		#mode 0 is relative, 1 is absolute and 2 is spindle. -1 is error.
+		self.move_mode=-2		#Mode 0 is relative and mode 1 is absolute. -1 is error.
 
 		#########  XPS Specific Calls  ##########
-		#self.x=xps.XPS()
-		#self.SocketID=x.TCP_ConnectToServer('192.168.0.254',5001,1)
-		#if self.SocketID == -1:
-		#	self.run_mode = -1
-		#self.Group = 'Spindle'
-		#self.Positioner = self.Group + '.Spindle'
+		'''
 
-		#self.profile=self.x.PositionerSGammaParameterGet(self.SocketID,self.Positioner)
-		#if self.profile[0] == 0:
-		#	pass
-		#else:
-		#	
+		self.home=[0]
+
+		self.x=xps.XPS()
+		self.SocketID=x.TCP_ConnectToServer('192.168.0.254',5001,1)
+		if self.SocketID == -1:
+			self.OnFail()
+			
+		self.Group = 'Group1'
+		self.Positioner = self.Group + '.Positioner'
+		
+		self.GKill=self.x.GroupKill(self.SocketId, self.Group)
+		if self.GKill[0] != 0:
+     		XPSErrorHandler(self.SocketID, self.GKill[0], 'GroupKill')
+     
+		self.GInit=self.x.GroupInitialize(self.SocketId, self.Group)
+		if GInit[0] != 0:
+     		XPSErrorHandler(self.SocketID, self.GInit[0], 'GroupInitialize')
+     
+		self.GHomeSearch=self.x.GroupHomeSearchAndRelativeMove(self.SocketId, self.Group,self.home)
+		if self.GHomeSearch != 0:
+     		XPSErrorHandler(self.SocketID, self.GHomeSearch[0], 'GroupHomeSearchAndRelativeMove')
+
+		self.profile=self.x.PositionerSGammaParameterGet(self.SocketID,self.Positioner)
+		if self.profile[0] != 0:
+			XPSErrorHandler(self.SocketID, self.GHomeSearch[0], 'PositionerSGammaParameterGet')	
+		'''	
 		#########################################
 
 		self.execute=wx.Button(self,label='Execute')
@@ -204,60 +249,49 @@ class Control(wx.Panel):
 	def __DoLayout(self):
 		'''A basic layout handler for Control panel.'''
 		sizer=wx.GridBagSizer()
-		sizer.Add(self.title,(0,0))
+		sizer.Add(self.title,(0,0),(1,3),wx.ALIGN_CENTER_HORIZONTAL|wx.CENTER)
 		sizer.Add(self.label_one,(1,1))
-		sizer.Add(self.label_two,(2,2))
+		sizer.Add(self.label_two,(2,2),(1,1),wx.ALIGN_CENTER_HORIZONTAL)
 		sizer.Add(self.label_three,(3,1))
 		sizer.Add(self.label_four,(4,2))
 		sizer.Add(self.mode_one,(1,0),(1,1),wx.ALIGN_CENTER_VERTICAL)
 		sizer.Add(self.mode_two,(2,0),(1,1),wx.ALIGN_CENTER_VERTICAL)
-		sizer.Add(self.mode_three,(3,0),(1,1),wx.ALIGN_CENTER_VERTICAL)
 		sizer.Add(self.speed,(2,1))
 		sizer.Add(self.position,(4,1))
-		sizer.Add(self.execute,(5,2))
+		sizer.Add(self.execute,(4,0))
 		self.SetSizer(sizer)
 
 	def OnButton(self,event):
 		'''Defining button functionality.'''
 		if self.move_mode == 0:
-			pass
-			#return=self.x.PositionerSGammaParameterSet(self.SocketID,self.Positioner,self.speed.GetValue(),self.profile[2],self.profile[3],self.profile[4])
-			#if return == 0:
-			#	move=self.x.GroupMoveRelative(self.SocketID,self.Group,self.position.GetValue())
-			#	if move == 0:
-			#		pass
-			#	else:
-			#		XPSErrorHandler(move)
-			#
-			#else:
-			#	XPSErrorHandler(return)			
+			'''
+			return=self.x.PositionerSGammaParameterSet(self.SocketID,self.Positioner,self.speed.GetValue(),self.profile[2],self.profile[3],self.profile[4])
+
+			if return != 0:
+				XPSErrorHandler(self.SocketID, return, 'PositionerSGammaParameterSet')	
+
+			else:
 				
+				move=self.x.GroupMoveRelative(self.SocketID,self.Group,[self.position.GetValue()])
+				if move != 0:
+					XPSErrorHandler(self.SocketID, move, 'GroupMoveRelative')
+						
+			'''
 		elif self.move_mode == 1:
-			pass
-			#return=self.x.PositionerSGammaParameterSet(self.SocketID,self.Positioner,self.speed.GetValue(),self.profile[2],self.profile[3],self.profile[4])
-			#if return == 0:
-			#	move=self.x.GroupMoveAbsolute(self.SocketID,self.Group,self.position.GetValue())
-			#	if move == 0:
-			#		pass
-			#	else:
-			#		XPSErrorHandler(move)
-			#
-			#else:
-			#	XPSErrorHandler(return)		
-		elif self.move_mode == 2:
-			pass
-			#return=self.x.PositionerSGammaParameterSet(self.SocketID,self.Positioner,self.speed.GetValue(),self.profile[2],self.profile[3],self.profile[4])
-			#if return == 0:
-			#	move=self.x.GroupJogModeEnable(self.SocketID,self.Group,self.position.GetValue()) ?????????
-			#	if move == 0:
-			#		pass
-			#	else:
-			#		XPSErrorHandler(move)
-			#
-			#else:
-			#	XPSErrorHandler(return)		
+			'''
+			return=self.x.PositionerSGammaParameterSet(self.SocketID,self.Positioner,self.speed.GetValue(),self.profile[2],self.profile[3],self.profile[4])
+
+			if return != 0:
+				XPSErrorHandler(self.SocketID, return, 'PositionerSGammaParameterSet')	
+
+			else:
+				move=self.x.GroupMoveAbsolute(self.SocketID,self.Group,[self.position.GetValue()])
+				if move != 0:
+					XPSErrorHandler(self.SocketID, move, 'GroupMoveRelative')
+						
+			'''
 		else:
-			print 'What even is this?'
+			result=wx.MessageBox('Button Malfunction in Control Panel.',style=wx.CENTER|wx.ICON_EXCLAMATION|wx.OK)
 		
 
 	def OnRadio(self,event):
@@ -265,18 +299,39 @@ class Control(wx.Panel):
 		if self.mode_one.GetValue()==True:
 			self.move_mode=0
 
-		elif self.mode_two.getValue()==True:
+		elif self.mode_two.GetValue()==True:
 			self.move_mode=1
-
-		elif self.mode_three.GetValue()==True:
-			self.move_mode=2
 
 		else:
 			self.move_mode=-1
+	
+	def OnFail(self):
+		result=wx.MessageBox('Connection to Newport Controller has failed at Control Panel.\nPlease Check IP and Port.',style=wx.CENTER|wx.ICON_EXCLAMATION|wx.OK)
+
+		if result == wx.OK:
+			sys.exit()
+
+		else:
+			sys.exit()
 
 	def Close(self):
-		print 'me'
+		
 		#self.x.TCP_CloseSocket(SocketID)
+			
+		print 'panel two closed'
+
+	def XPSErrorHandler(self,socket,code,name):
+		if code != -2 and code != -108:
+			error=self.x.ErrorStringGet(socket,code)
+			if error[0] != 0:
+				choice=wx.MessageBox(name +' : ERROR '+ str(code),style=wx.CENTER|wx.ICON_EXCLAMATION|wx.OK)
+			else:
+				choice=wx.MessageBox(name +' : '+ error[1],style=wx.CENTER|wx.ICON_EXCLAMATION|wx.OK)
+		else:
+			if code == -2:
+				choice=wx.MessageBox(name +' : TCP timeout',style=wx.CENTER|wx.ICON_EXCLAMATION|wx.OK)
+			elif code == -108:
+				choice=wx.MessageBox(name +' : The TCP/IP connection was closed by an administrator',style=wx.CENTER|wx.ICON_EXCLAMATION|wx.OK)
 
 if __name__=='__main__':
 	app=KMirrorApp(False)	
