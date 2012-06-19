a010615dab9d095d01d398415fab18b6ea2b9d42
matt <matt@ubuntu.ubuntu-domain>
2012-06-19 11:16:45 -0600

    Starting a new branch for matts weekly work. This commit includes a module and a readme.

---
+++ newportfunctions.py
@@ -0,0 +1,12 @@
+#!usr/bin/env python
+
+# newportfunctions.py
+# Matt Napolitano
+# Created 06/19/2012
+
+# This is designed to be a module used by the NESSI GUI to interact with the newport controller.
+
+import XPS_C8_drivers as xps
+
+x=xps.XPS()
+
