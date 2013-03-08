ea0e9e3abcb7eb640186206784d8cf71084f70c2
Luke Schmidt <lschmidt@NESSI.(none)>
2013-03-08 12:07:59 -0700

    added option to initialize for either controller.
    added completion function that will wait for any return confirmation.
    
    began move_relative function

--- tl.py
+++ tl.py
@@ -9,10 +9,12 @@ DEBUG = True
 class tlabs:
     """Represents the Thorlabs TDC001 controller."""
     
-    def __init__(self, port='/dev/ttyUSB0'):
+    def __init__(self, port=0):
         """Performs necessary startup procedures."""
-        self.__port = port
-
+        if port == 0:
+            self.__port = '/dev/ttyREI12'
+        if port == 1:
+            self.__port = '/dev/ttyREI34'
         # Establish a connection to the KMtronic relay board
         self.ser = serial.Serial(port=self.__port, baudrate=115200, bytesize=serial.EIGHTBITS,
                                  parity=serial.PARITY_NONE, stopbits=serial.STOPBITS_ONE,
@@ -22,17 +24,14 @@ class tlabs:
         """Perform cleanup operations."""
         self.ser.close()
 
-    def flash(self):
+    def identify(self):
         """Flash light on front."""
         self.ser.write('\x23\x02\x00\x00\x50\x01')
         if DEBUG: print "DEBUG: Flash"
-
-        return True
     
-    def home(self):
-        """Home stage."""
-        self.ser.write('\x43\x04\x01\x00\x50\x01')
-        if DEBUG: print "DEBUG: Home Stage"
+    def completion(self):
+        """Generic function that waits to read a completion command such as, home
+        finished, move complete, etc."""
         now = time.time()
         while True:
             ch = self.ser.read(self.ser.inWaiting())
@@ -41,4 +40,25 @@ class tlabs:
                     print 'ch is:', ch, sys.getsizeof(ch) #prints size in bytes
             if time.time() > now+30.0:
                 break
+    
+    def home(self):
+        """Home stage."""
+        self.ser.write('\x43\x04\x01\x00\x50\x01')
+        if DEBUG: print "DEBUG: Home Stage"
+        self.completion(self)
             
+    def move_relative(self,distance=0):
+        """Move the stage a relative distance.  Input is in microns, signed integer.
+        The conversion factor is:  34,304 encoder counts/revolution of lead screw, 
+        which moves the lead screw by 1mm, or 1mm/34,304 counts = 29nm/encoder count.
+        The command expects input in encoder counts."""
+        #convert distance in um to counts, make sure it is an integer.
+        counts = int(distance * 0.02915111) #um per count
+        #convert to hex
+        
+        #convert to input format, least sig digit first
+        
+        #write move command
+        if DEBUG: print 'Start relative move by: ', counts, ' counts.'
+        #wait for completion command
+        self.completion(self)    
