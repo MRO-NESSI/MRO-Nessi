188cf021767bd73238fcf1ea1709449125f2aa67
Luke Schmidt <lschmidt@NESSI.(none)>
2013-03-08 16:40:53 -0700

    added more functions and cleaned up reading return messages.
    don't use absolute or relative moves, yet, still need to figure out
    how to correctly convert a distance in um to the proper hex code.

--- tl.py
+++ tl.py
@@ -3,6 +3,7 @@
 import serial
 import time
 import sys
+from struct import pack, unpack
 
 DEBUG = True
 
@@ -34,18 +35,17 @@ class tlabs:
         finished, move complete, etc."""
         now = time.time()
         while True:
-            ch = self.ser.read(self.ser.inWaiting())
-            if DEBUG: 
-                if ch != '': 
-                    print 'ch is:', ch, sys.getsizeof(ch) #prints size in bytes
-            if time.time() > now+30.0:
+            ch = self.ser.readline()
+            if ch != '': 
                 break
+        
+        return ch.encode('hex')
     
     def home(self):
         """Home stage."""
         self.ser.write('\x43\x04\x01\x00\x50\x01')
-        if DEBUG: print "DEBUG: Home Stage"
-        self.completion(self)
+        if DEBUG: print 'DEBUG: Home Stage'
+        return self.completion()
             
     def move_relative(self,distance=0):
         """Move the stage a relative distance.  Input is in microns, signed integer.
@@ -53,12 +53,36 @@ class tlabs:
         which moves the lead screw by 1mm, or 1mm/34,304 counts = 29nm/encoder count.
         The command expects input in encoder counts."""
         #convert distance in um to counts, make sure it is an integer.
-        counts = int(distance * 0.02915111) #um per count
+        counts = int(distance / 0.02915111) #um per count
         #convert to hex
-        
-        #convert to input format, least sig digit first
-        
+        counts = pack('<i', counts)
         #write move command
+        self.ser.write('\x48\x04\x06\x00\x50\x01\x01\x00' + counts)
         if DEBUG: print 'Start relative move by: ', counts, ' counts.'
         #wait for completion command
-        self.completion(self)    
+        return self.completion()  
+        
+    def move_absolute(self,position=0):
+        """Move the stage to an absolute position.  Input is in microns, signed integer.
+        The conversion factor is:  34,304 encoder counts/revolution of lead screw, 
+        which moves the lead screw by 1mm, or 1mm/34,304 counts = 29nm/encoder count.
+        The command expects input in encoder counts."""
+        #convert distance in um to counts, make sure it is an integer.
+        counts = int(position / 0.02915111) #um per count
+        #convert to hex
+        counts = pack('<i', counts)
+        #write move command
+        self.ser.write('\x53\x04\x06\x00\x50\x01\x01\x00' + counts)
+        if DEBUG: print 'Start absolute move to: ', counts, ' counts.'
+        #wait for completion command
+        return self.completion()
+        
+    def get_status_update(self):
+        """Get the current status of the actuator, including positin, encoder count,
+        and other status messages, such as limit switch activated, in motion, etc."""
+        #send status update request
+        self.ser.write('\x90\x04\x01\x00\x50\x01')
+        if DEBUG: print 'Status update requested'
+        return self.completion()
+        
+        
