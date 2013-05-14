7d558c573e3fdaeb77b5749c9a53fd4409d7a7a2
Izzy Cecil <lorr.cecil@gmail.com>
2013-05-14 16:31:36 -0600

    DOC: thorlands documentation.

--- Z806V/tl.py
+++ Z806V/tl.py
@@ -7,20 +7,17 @@ from handlers import *
 
 DEBUG = True
 
-class tlabs:
-    """Represents the Thorlabs TDC001 controller."""
-
-    #TODO: This should be a simplified dynamic structure
-    tlabs_commands = {
-        #COMMAND           #SIGNAL HEADER                     #Has exit status
-        'Identify'      : ('\x23\x02\x00\x00\x50\x01',         False),
-        'Home'          : ('\x43\x04\x01\x00\x50\x01',         True),
-        'Status'        : ('\x90\x04\x01\x00\x50\x01',         True),
-        'Move Absolute' : ('\x48\x04\x06\x00\x50\x01\x01\x00', True),
-        'Move Relative' : ('\x48\x04\x06\x00\x50\x01\x01\x00', True),
-        }
-
-    #IMP: Consider makeing hidden?
+class TLabs:
+    """Represents a Thorlabs TDC001 controller."""
+    
+    commands = { 
+        #COMMAND          #SIGNAL HEADER           #Has exit status
+        'Identify'     : ('\x23\x02\x00\x00\x50\x01',         False), 
+        'Home'         : ('\x43\x04\x01\x00\x50\x01',         True), 
+        'Status'       : ('\x90\x04\x01\x00\x50\x01',         True), 
+        'Move Absolute': ('\x48\x04\x06\x00\x50\x01\x01\x00', True),
+        'Move Relative': ('\x48\x04\x06\x00\x50\x01\x01\x00', True),
+    }
     ports = {0: '/dev/ttyREI12', 1: '/dev/ttyREI34'}
 
     def __init__(self, port=0):
@@ -30,8 +27,10 @@ class tlabs:
         except:
             raise InvalidPortException()
         # Establish a connection to the KMtronic relay board
-        self.ser = serial.Serial(port=self.__port, baudrate=115200, bytesize=serial.EIGHTBITS,
-                                 parity=serial.PARITY_NONE, stopbits=serial.STOPBITS_ONE,
+        self.ser = serial.Serial(port=self.__port, baudrate=115200,
+                                 bytesize=serial.EIGHTBITS,
+                                 parity=serial.PARITY_NONE,
+                                 stopbits=serial.STOPBITS_ONE,
                                  timeout=1, rtscts=True)
         
     def __del__(self):
@@ -39,8 +38,17 @@ class tlabs:
         self.ser.close()
 
     def read_exit_status(self):
-        """Generic function that waits to read a completion command such as, home
-        finished, move complete, etc."""
+        """Reads exit status from controller.
+
+        Generic function to read from the serial port.
+        Reads untill an empty line is read.
+
+        returns (hex, characters)
+        """
+        """
+        TODO: This only returns one line. It may need to be
+        examined
+        """
         while True:
             ch = self.ser.readline()
             if ch != '': 
@@ -48,19 +56,30 @@ class tlabs:
         return ch.encode('hex'), ch
 
     def send_command(self, command):
-        command = tlabs.tlabs_commands[command]
+        """Send a command to the controller.
+        
+        returns the exit status, if the command returns one. Otherwise
+        none.
+        """
+        command = tlabs.commands[command]
         self.ser.write(command[0])
         return self.read_exit_status() if command[1] else None
             
     def move_relative(self,distance=0):
-        """Move the stage a relative distance.  Input is in microns, signed integer.
-        The conversion factor is:  34,304 encoder counts/revolution of lead screw, 
-        which moves the lead screw by 1mm, or 1mm/34,304 counts = 29nm/encoder count.
-        The command expects input in encoder counts."""
+        """Move the stage a relative distance.  
+
+        distance --- in microns (signed int)
+        
+        returns exit status.
+        """
+        """
+        Conversion factors:
+          34,304 (encoder counts)/(revolution of lead screw) 
+          29 (nm)/(encoder count)
+        The command expects input in encoder counts.
+        """
         #get current position to make sure move doesn't drive to limit
-        
-        #if relative move will place it at a limit, move to just before the limit
-        
+        #TODO:IS THAT NECESSARY?
         #convert distance in um to counts, make sure it is an integer.
         intcounts = int(distance / 0.02915111) #um per count
         #convert to hex
@@ -73,10 +92,18 @@ class tlabs:
         return self.read_exit_status()
         
     def move_absolute(self,position=0):
-        """Move the stage to an absolute position.  Input is in microns, signed integer.
-        The conversion factor is:  34,304 encoder counts/revolution of lead screw, 
-        which moves the lead screw by 1mm, or 1mm/34,304 counts = 29nm/encoder count.
-        The command expects input in encoder counts."""
+        """Move the stage an absolute position.
+
+        postition --- in microns (signed int)
+        
+        returns exit status.
+        """
+        """
+        Conversion factors:
+          34,304 (encoder counts)/(revolution of lead screw) 
+          29 (nm)/(encoder count)
+        The command expects input in encoder counts.
+        """
         #convert distance in um to counts, make sure it is an integer.
         intcounts = int(position / 0.02915111) #um per count
         #convert to hex
