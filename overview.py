9bdbe5f57ddeca4d86d85925b311300e861a7768
Izzy Cecil <lorr.cecil@gmail.com>
2013-05-27 11:01:22 -0600

    Uncommented out the focus motors

--- overview.py
+++ overview.py
@@ -15,15 +15,13 @@ class OverviewPanel(wx.Panel):
         # Attributes
         self.Schematic = SchematicPanel(self)
         self.Mask = WheelPanel(self, 'mask')
-#        self.FilterOne = FilterPanelOne(self)
-#        self.FilterTwo = FilterPanelTwo(self)
         self.FilterOne = WheelPanel(self, 'filter1')
         self.FilterTwo = WheelPanel(self, 'filter2')
         self.Grism = WheelPanel(self, 'grism')
         self.Kmirror = KmirrorPanel(self)
         self.GuideInfo = GuideInfoPanel(self)
-#        self.FocusREI12 = FocusREIPanel(self, 'Focus REI-1-2', 0)
-#        self.FocusREI34 = FocusREIPanel(self, 'Focus REI-3-4', 1)
+        self.FocusREI12 = FocusREIPanel(self, 'Focus REI-1-2', 0)
+        self.FocusREI34 = FocusREIPanel(self, 'Focus REI-3-4', 1)
         
         self.__DoLayout()
 
@@ -43,10 +41,10 @@ class OverviewPanel(wx.Panel):
         
         # Add controls to gridbag
         sizer.Add(self.Kmirror, pos=(0,0), span=(1,1), flag=wx.LEFT|wx.ALIGN_CENTER_VERTICAL)
-#        sizer.Add(self.FocusREI12, pos=(0,1), span=(1,1), flag=wx.LEFT|wx.ALIGN_BOTTOM)
+        sizer.Add(self.FocusREI12, pos=(0,1), span=(1,1), flag=wx.LEFT|wx.ALIGN_BOTTOM)
         sizer.Add(self.GuideInfo, pos=(1,2), span=(1,1), flag=wx.LEFT|wx.ALIGN_CENTER_VERTICAL)
         sizer.Add(self.Schematic, pos=(1,0), span=(6,2), flag=wx.LEFT|wx.ALIGN_CENTER_VERTICAL)
-#        sizer.Add(self.FocusREI34, pos=(2,2), span=(1,1), flag=wx.LEFT|wx.ALIGN_BOTTOM)
+        sizer.Add(self.FocusREI34, pos=(2,2), span=(1,1), flag=wx.LEFT|wx.ALIGN_BOTTOM)
         sizer.Add(self.Mask, pos=(3,2), span=(1,1), flag=wx.LEFT|wx.ALIGN_CENTER_VERTICAL)
         sizer.Add(self.FilterOne, pos=(4,2), span=(1,1), flag=wx.LEFT|wx.ALIGN_CENTER_VERTICAL)
         sizer.Add(self.FilterTwo, pos=(5,2), span=(1,1), flag=wx.LEFT|wx.ALIGN_CENTER_VERTICAL)
