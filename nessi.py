3c37c5bc672cf6f929bd9db0c3ba16d02296ee16
Izzy Cecil <lorr.cecil@gmail.com>
2013-05-28 14:47:18 -0600

    FIX: nessi.py status updates

--- nessi.py
+++ nessi.py
@@ -9,6 +9,7 @@
 __author__ = 'Luke Schmidt, Matt Napolitano, Tyler Cecil'
 __date__ = '2013'
 
+from random import randint
 import sys
 import time
 import wx
@@ -70,7 +71,7 @@ class MainNessiFrame(wx.Frame):
 
         #Build Frame
         self.create_menus()
-        self.CreateStatusBar()
+        self.statusbar=self.CreateStatusBar()
         
         #Init Notebook panel
         p = wx.Panel(self)
@@ -101,10 +102,12 @@ class MainNessiFrame(wx.Frame):
         sizer = wx.BoxSizer()
         sizer.Add(nb, 1, wx.EXPAND)
         p.SetSizer(sizer)
-        
+
+        self.statusbar.SetStatusText("Welcome to NESSI!")
+
     def change_statusbar(self, msg):
-        print 'I did a very cool thing'
-        self.SetStatusText(msg.data)
+        print msg.data
+        self.statusbar.SetStatusText(msg.data)
         
     def create_menus(self):
         menuBar = wx.MenuBar()
