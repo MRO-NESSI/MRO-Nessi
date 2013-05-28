083a9aee7865b6f341278ccc6a6ab3523c9fae58
Matt Napolitano <mnapolitano@NESSI.(none)>
2013-05-28 16:54:48 -0600

    Adding an emergency panel for nessi.py, updating nessisettings.ini and newport.py.

--- actuators/newport.py
+++ actuators/newport.py
@@ -20,32 +20,45 @@ for i in range(int(cfg['general']['sockets'])):
 		print 'Error, Sockets not opened.'
 
 
-def XPSErrorHandler(controller,socket,code,name):
-	'''This is a general error handling function for the newport controller functions. First the function checks for errors in communicating with the controller, then it fetches the error string and displays it in a message box.  If the error string can not be found it will print the error code for lookup by the user.'''
+def XPSErrorHandler(controller, socket, code, name):
+	'''
+This is a general error handling function for the newport controller functions. 
+First the function checks for errors in communicating with the controller, then it fetches the error string and displays it in a message box.  
+If the error string can not be found it will print the error code for lookup by the user.
+
+	Inputs: controller, socket, code, name.
+
+	controller:	[xps]	Which instance of the XPS controller to use.
+	socket:		[int]	Which socket to use to communicate with the XPS controller.
+	code:		[int]	The error code returned by the function that called this function.
+	name:		[str]	The name of the function that called this function.
+'''
 
 	# This checks to see if the error is in communication with the controller.
 	if code != -2 and code != -108:
 
 		# Getting the error string.
-		error=controller.ErrorStringGet(socket,code)
+		error = controller.ErrorStringGet(socket, code)
 		# If the error string lookup fails, this message will display with the error code.
 		if error[0] != 0:
-			choice=wx.MessageBox(name +' : ERROR '+ str(code),style=wx.CENTER|wx.ICON_EXCLAMATION|wx.OK)
+			choice=wx.MessageBox(name + ' : ERROR '+ str(code), style=wx.CENTER|wx.ICON_EXCLAMATION|wx.OK)
 		# This displays the error string.
 		else:
 			print error[1] #choice=wx.MessageBox(name +' : '+ error[1],style=wx.CENTER|wx.ICON_EXCLAMATION|wx.OK)
 	# This code handles the case where the connection to the controller fails after initial contact.
 	else:
 		if code == -2:
-			choice=wx.MessageBox(name +' : TCP timeout',style=wx.CENTER|wx.ICON_EXCLAMATION|wx.OK)
+			choice=wx.MessageBox(name + ' : TCP timeout', style=wx.CENTER|wx.ICON_EXCLAMATION|wx.OK)
 		elif code == -108:
-			choice=wx.MessageBox(name +' : The TCP/IP connection was closed by an administrator',style=wx.CENTER|wx.ICON_EXCLAMATION|wx.OK)
+			choice=wx.MessageBox(name + ' : The TCP/IP connection was closed by an administrator', style=wx.CENTER|wx.ICON_EXCLAMATION|wx.OK)
 		
 
 #@threaded
-def NewportWheelThread(controller,wheel,socket,current,position,home):
+def NewportWheelThread(controller, wheel, socket, current, position, home):
 	'''
 A thread that initiates a move in the dewar and then monitors The Newport GPIO for a bit flip that indicates the motor needs to be stopped.
+If the motion fails the function will print a message to the terminal. (to be changed)
+If the motion succedes the function will log a message. (to be added when working logging is added)
 
 	Inputs:	controller, name, socket, position, home.
 
@@ -57,24 +70,22 @@ A thread that initiates a move in the dewar and then monitors The Newport GPIO f
 	home:		[bool]	Determines whether the thread will find the home position or a different position.
 '''
 	# Initializing variables.
-	controller=controller
-	group=cfg[wheel]['group']
-	socket=socket
-	state=0
+	group = cfg[wheel]['group']
+	state = 0
 	# Different initializations depending on whether it is homing or not.
 	if home == True:
-		val=int(cfg[wheel]['home']['val'])
-		bit=int(cfg[wheel]['home']['bit'])
-		diff=1
+		val = int(cfg[wheel]['home']['val'])
+		bit = int(cfg[wheel]['home']['bit'])
+		diff = 1
 	else:
-		val=int(cfg[wheel]['position']['val'])
-		bit=int(cfg[wheel]['position']['bit'])
+		val = int(cfg[wheel]['position']['val'])
+		bit = int(cfg[wheel]['position']['bit'])
 		# self.diff is how many positions away from current the target position is.
-		diff=(int(cfg[wheel]['slots'])-current+position)%int(cfg[wheel]['slots'])
+		diff = (int(cfg[wheel]['slots']) - current + position) % int(cfg[wheel]['slots'])
 
 	if current != position:
 		# Starting motion.
-		Gset=controller.GroupSpinParametersSet(socket,cfg[wheel]['group'],100,200)
+		Gset = controller.GroupSpinParametersSet(socket, cfg[wheel]['group'], 100, 200)
 		# Checking if the motion command was sent correctly.
 		# If so then the GPIO checking begins.
 		if Gset[0] != 0:
@@ -89,10 +100,10 @@ A thread that initiates a move in the dewar and then monitors The Newport GPIO f
 				time.sleep(.15)
 				value = controller.GPIODigitalGet(socket, 'GPIO4.DI')
 				if value[0] != 0:
-					XPSErrorHandler(controller,socket,value[0],'GPIODigitalGet')
-				elif int(format(value[1],"016b")[::-1][bit]) == val:
+					XPSErrorHandler(controller, socket, value[0], 'GPIODigitalGet')
+				elif int(format(value[1], "016b")[::-1][bit]) == val:
 					time.sleep(1)
-					state = state+1
+					state = state + 1
 				else:
 					pass
 			# This loop counts the switch flip with no delay so the motor can be stopped at the position.
@@ -100,50 +111,84 @@ A thread that initiates a move in the dewar and then monitors The Newport GPIO f
 				time.sleep(.15)
 				value = controller.GPIODigitalGet(socket, 'GPIO4.DI')
 				if value[0] != 0:
-					XPSErrorHandler(controller,socket,value[0],'GPIODigitalGet')
-				elif  int(format(value[1],"016b")[::-1][bit]) == val:
-					state = state+1
+					XPSErrorHandler(controller, socket, value[0], 'GPIODigitalGet')
+				elif  int(format(value[1], "016b")[::-1][bit]) == val:
+					state = state + 1
 				else:
 					pass
 			# Stopping the motor
 			stop=x.GroupSpinModeStop(socket, group, 800)
 			if stop[0] != 0:
-				XPSErrorHandler(controller,socket,stop[0],'GroupSpinModeStop')
+				XPSErrorHandler(controller, socket, stop[0], 'GroupSpinModeStop')
 			# Checking to be sure the motor is in a valid position.
-			elif int(format(value[1],"016b")[::-1][bit]) != val:		
+			elif int(format(value[1], "016b")[::-1][bit]) != val:		
 				print 'motion failed, home and then reinitiate move'
 			else:
 				print 'motion succeded'
-				Publisher().sendMessage((group+'_state'),state)
+				Publisher().sendMessage((group + '_state'), state)
 	else:
 		pass
 
 
-def NewportInitialize(controller,motor,socket,home_position):
+def NewportInitialize(controller, motor, socket, home_pos):
 	'''
 An initialization function for any motor controlled by the XPS controller.
+This function returns nothing if succesful and calls XPSErrorHandler otherwise.
 
-	Inputs: controller, motor, socket, home_position
+	Inputs: controller, motor, socket, home_position.
 	
 	controller:	[xps]	Which instance of the XPS controller to use.
-	wheel:		[str]	The name of the motor that is being used.  This is for config file purposes.
+	motor:		[str]	The name of the motor that is being used.  This is for config file purposes.
 	socket:		[int]	Which socket to use to communicate with the XPS controller.
-	current:	[int]	What position the motor is currently at.
-	position:	[int]	What position the motor should move to.
-	home:		[bool]	Determines whether the thread will find the home position or a different position.
+	home_pos:	[int]	Determines whether the thread will find the home position or a different position.
 '''
-	GKill=controller.GroupKill(open_sockets[0], cfg[motor]['group'])	
+	GKill = controller.GroupKill(socket, cfg[motor]['group'])	
 	if GKill[0] != 0:
-		XPSErrorHandler(open_sockets[0], GKill[0], 'GroupKill')
-	GInit=controller.GroupInitialize(open_sockets[0], cfg[motor]['group'])
+		XPSErrorHandler(socket, GKill[0], 'GroupKill')
+
+	GInit = controller.GroupInitialize(socket, cfg[motor]['group'])
 	if GInit[0] != 0:
-		XPSErrorHandler(open_sockets[0], GInit[0], 'GroupInitialize')
-	GHomeSearch=controller.GroupHomeSearchAndRelativeMove(open_sockets[0], cfg[motor]['group'],[home_position])
-	print GHomeSearch
+		XPSErrorHandler(socket, GInit[0], 'GroupInitialize')
+
+	GHomeSearch = controller.GroupHomeSearchAndRelativeMove(socket, cfg[motor]['group'],[home_pos])
 	if GHomeSearch[0] != 0:
-		XPSErrorHandler(open_sockets[0], GHomeSearch[0], 'GroupHomeSearchAndRelativeMove')
+		XPSErrorHandler(socket, GHomeSearch[0], 'GroupHomeSearchAndRelativeMove')
 
+
+def NewportKmirrorMove():
+	'''
+This function moves the k-mirror to a choosen position at 10 deg/s.
+
+	Inputs: controller, socket, motor, jog_state, position.
+'''
+
+
+def NewportKmirrorRotate(controller, socket, motor, jog_state, velocity):
+	'''
+This function prepares the motor for continuous rotation if it isn't already prepared and then sets a choosen velocity.
+
+	Inputs: controller, socket, motor, jog_state, velocity.
+
+	controller:	[xps]	Which instance of the XPS controller to use.
+	socket:		[int]	Which socket to use to communicate with the XPS controller.
+	motor:		[str]	Which motor is being controlled.  This is for config file purposes.
+	jog_state:	[bool]	Whether or not the motor in question is already configured for continuous rotation. 
+	velocity:	[float]	What value to set the rotational velocity to in deg/s.
+'''
+	if jog == False:
+		Gmode = controller.GroupJogModeEnable(socket, cfg[motor]['group'])
+		if Gmode[0] != 0:
+   			XPSErrorHandler(controller, socket, Gmode[0], 'GroupJogModeEnable')
+	else:
+		pass
+	
+	GJog = controller.GroupJogParametersSet(socket, cfg[motor]['group'], [velocity],[400])
+	if GJog[0] != 0:
+		XPSErrorHandler(controller, socket, GJog[0], 'GroupJogParametersSet')
+	
+	
+
+# Test code to be removed later
 if __name__ == '__main__':
-
-	NewportInitialize(x,'grism',open_sockets[0],0)
-	NewportWheelThread(x,'grism',open_sockets[0],1,4,True)
+	NewportInitialize(x, 'grism', open_sockets[0], 0)
+	NewportWheelThread(x, 'grism', open_sockets[0], 1, 4, True)
---
+++ emergency.py
@@ -0,0 +1,35 @@
+import wx
+
+class EmergencyPanel(wx.Panel):
+    def __init__(self, parent, controller, socket, *args, **kwargs):
+        super(EmergencyPanel, self).__init__(parent)
+        
+        self.controller = controller
+        self.socket = socket
+        # Attributes
+        self.t = wx.StaticText(self, -1, "Emergency", (40,40))
+        self.emergency = wx.Button(self,label='KILL ALL', size=(400,200))
+        self.emergency.SetBackgroundColour(wx.Colour(255,0, 0))
+        self.emergency.ClearBackground()
+        self.emergency.Refresh() 
+        self.font=wx.Font(pointSize = 40, family = wx.FONTFAMILY_DECORATIVE, style = wx.FONTSTYLE_NORMAL, weight = wx.FONTWEIGHT_BOLD)
+        self.emergency.SetFont(self.font)  
+        self.Bind(wx.EVT_BUTTON,self.OnButton)
+
+        self.__DoLayout()
+
+    def __DoLayout(self):
+        sbox = wx.StaticBox(self, label="")
+        boxSizer = wx.StaticBoxSizer(sbox, wx.HORIZONTAL)
+        sizer = wx.GridBagSizer(vgap=2, hgap=2)
+        
+        # Add controls to gridbag
+        sizer.Add(self.t, pos=(0,0), span=(1,1), flag=wx.LEFT|wx.ALIGN_CENTER_VERTICAL)
+        sizer.Add(self.emergency, pos=(1,0),span=(1,1), flag=wx.ALIGN_CENTER_HORIZONTAL|wx.CENTER)
+                
+        # Add the grid bag to the static box and make everything fit
+        boxSizer.Add(sizer, wx.EXPAND|wx.CENTER)
+        self.SetSizerAndFit(boxSizer)
+
+    def OnButton(self, event):
+        pass
