2cc2d6121ebdb6a4e33f245cbdca9a4a8545e50e
Izzy Cecil <lorr.cecil@gmail.com>
2013-05-30 10:41:18 -0600

    Moved timeout decorator

--- threadtools.py
+++ threadtools.py
@@ -7,6 +7,30 @@ from types import FunctionType, MethodType
 
 __all__ = ['callafter', 'synchfunct', 'ClassSynchronizer']
 
+class TimeoutError(Exception):
+    def __init__(self, value = "Timed Out"):
+        self.value = value
+    def __str__(self):
+        return repr(self.value)
+
+def timeout(seconds_before_timeout):
+    def decorate(f):
+        def handler(signum, frame):
+            raise TimeoutError()
+        def new_f(*args, **kwargs):
+            old = signal.signal(signal.SIGALRM, handler)
+            signal.alarm(seconds_before_timeout)
+            try:
+                result = f(*args, **kwargs)
+            finally:
+                signal.signal(signal.SIGALRM, old)
+            signal.alarm(0)
+            return result
+        new_f.func_name = f.func_name
+        return new_f
+    return decorate
+
+
 def run_async(func):
     from threading import Thread
     from functools import wraps
