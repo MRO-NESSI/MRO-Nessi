5770a0aff19d27533ab2ecc282479019f5fcb5fc
Izzy Cecil <tcecil@mro.nmt.edu>
2013-06-13 16:11:33 -0600

    Beggining of OnGuide method

--- guidepaneltab/guidingpanel.py
+++ guidepaneltab/guidingpanel.py
@@ -9,13 +9,15 @@ class GuidingPanel(wx.Panel):
         self.star2 = wx.StaticText(self, label="Star 2: ")
         self.star1_xy = wx.StaticText(self, label="(x.000,y.000)")
         self.star2_xy = wx.StaticText(self, label="(x.000,y.000)")
-        self.guide = wx.ToggleButton(self, 1, size=(100,-1), label="Guiding")
+        self.guide = wx.ToggleButton(self, 1, size=(100,-1), label="Start Guiding")
         self.log_onoff = wx.CheckBox(self, -1, 'Guiding Log On', (10,10))
         self.guide_state = self.guide.GetValue()
         self.cadence_text = wx.StaticText(self, label="Cadence (s): ")
         self.cadence = wx.TextCtrl(self, -1, '10.0', size=(50,-1), style=wx.TE_NO_VSCROLL)
 
         self._DoLayout()
+
+        self.guide.Bind(wx.EVT_TOGGLEBUTTON, self.OnGuide)
 
     def _DoLayout(self):
         sz = wx.GridBagSizer(vgap=3, hgap=3)
@@ -30,3 +32,12 @@ class GuidingPanel(wx.Panel):
         sz.Add(self.cadence, pos=(4,1), span=(1,1), flag=wx.ALIGN_CENTER_VERTICAL)
 
         self.SetSizerAndFit(sz)
+
+    def OnGuide(self, event):
+        if self.guide.GetValue():
+            self.guide.SetLabel("Stop Guiding")
+            self.guide.SetForegroundColour((34,139,34))
+        else:
+            self.guide.SetLabel("StartGuiding")
+            self.guide.SetForegroundColour((0,0,0))
+
