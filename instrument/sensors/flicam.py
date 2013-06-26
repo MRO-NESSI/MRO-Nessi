24ebffa4751a35a6f18c48b958b2e68dc2a50c92
Izzy Cecil <tcecil@mro.nmt.edu>
2013-06-26 17:37:25 -0600

    flicam.py a valid instroment component.

--- instrument/sensors/flicam.py
+++ instrument/sensors/flicam.py
@@ -1,42 +1,141 @@
 from time import sleep
-from threading import Lock
+
 import pyfli as p
 
-class FLICam(object):
+from instrument import InstrumentComponent, InstrumentError
+
+class FLICam(InstrumentComponent):
     """Represents an FLI USB Camera."""
 
-    def __init__(self, cam=0):
-        self._path = p.FLIList('usb', 'camera')[cam][0]
-        self._id   = p.FLIOpen(self._path, 'usb', 'camera')
-        self.lock  =  Lock()
+    def __init__(self, instrument, cam=0):
+        """Connects to a new FLI Camera. 
+
+        Arguments:
+            instrument -- Copy of the NESSI instrument
+            cam        -- int representing which camera to use
+                          (defaults to 0).
+
+        Raises InstrumentError
+        """
+
+        super(FLICam, self).__init__(instrument)
+
+        try:
+            self._path = p.FLIList('usb', 'camera')[cam][0]
+            self._id   = p.FLIOpen(self._path, 'usb', 'camera')
+        except Exception as e:
+            raise InstrumentError('An error occurred initializing the'
+                                  ' Guide cam. Is it plugged in and'
+                                  ' powered on.\n The following error'
+                                  ' was raised...\n %s' % repr(e))
     
     def setExposure(self, time):
-        with self.lock:
-            p.setExposureTime(self._id, time)
+        """Sets the exposure time of the FLI Camera.
+
+        Arguments:
+            time --- new exposure time
+
+        Raises:
+            InstrumentError
+        
+        returns None
+        """
+        try:
+            with self.lock:
+                p.setExposureTime(self._id, time)
+        except Exception as e:
+            raise InstrumentError('An error occured communicating with the'
+                                  ' guide cam!\n The following error was'
+                                  ' raised...\n %s' % repr(e))
 
     def takePicture(self):
-        with self.lock:
-            p.exposeFrame(self._id)
-            wait = 100
-            while wait != 0:
-                wait = p.getExposureStatus(self._id)
-                sleep(wait/1000)
-            return p.grabFrame(self._id)
+        """Takes a picture with the guide cam.
+
+        Arguments:
+
+        Raises:
+            InstrumentError
+        
+        returns numpy array of image
+        """
+
+        try:
+            with self.lock:
+                p.exposeFrame(self._id)
+                wait = 100
+                while wait != 0:
+                    wait = p.getExposureStatus(self._id)
+                    sleep(wait/1000)
+        except Exception as e:
+            raise InstrumentError('An error occured communicating with the'
+                                  ' guide cam!\n The following error was'
+                                  ' raised...\n %s' % repr(e))
+            
+        return p.grabFrame(self._id)
 
     def getTemperature(self):
-        with self.lock:
-            return p.getTemperature(self._id)
+        """Returns the temperature of the FLI Camera.
 
+        Arguments:
+        
+        Raises:
+            InstrumentError
+        
+        returns string representation of the temperature, in Celsius.
+        """
+        try:
+            with self.lock:
+                return p.getTemperature(self._id)
+        except Exception as e:
+            raise InstrumentError('An error occured communicating with the'
+                                  ' guide cam!\n The following error was'
+                                  ' raised...\n %s' % repr(e))
+            
     def setTemperature(self, temp):
-        with self.lock:
-            return p.setTemperature(self._id, temp)
+        """Set the temperature of the FLI Camera.
+
+        Arguments:
+            temp -- new temperature of the camera, in Celsius
+
+        Raises:
+            InstrumentError
+
+        returns whatever pyfli.setTeperature returns... ?
+        """
+        try:
+            with self.lock:
+                return p.setTemperature(self._id, temp)
+        except Exception as e:
+            raise InstrumentError('An error occured communicating with the'
+                                  ' guide cam!\n The following error was'
+                                  ' raised...\n %s' % repr(e))           
 
     def setBinning(self, hBin, vBin):
-        with self.lock:
-            p.setHBin(self._id, hBin)
-            p.setVBin(self._id, vBin)
+        """Set the binning of the FLI Camera.
+        
+        Arguments:
+            hBin  -- horizontal binning
+            vBin  -- vertical binning
+
+        Raises:
+            InstrumentError
+
+        returns None
+        """
+        try:
+            with self.lock:
+                p.setHBin(self._id, hBin)
+                p.setVBin(self._id, vBin)
+        except Exception e:
+            raise InstrumentError('An error occured communicating with the'
+                                  ' guide cam!\n The following error was'
+                                  ' raised...\n %s' % repr(e))           
         
     def __del__(self):
         with self.lock:
-            p.FLIClose(sef._id)
+            p.FLIClose(self._id)
+
+    def kill(self):
+        """Closes connection. Called by a kill_all"""
+        p.FLIClose(self._id)
         
